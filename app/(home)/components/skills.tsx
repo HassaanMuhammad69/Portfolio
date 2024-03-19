f754135a1c31cde374c1c9b0ce583c4c67931501
Hassaan Muhammad <hassaans299@gmail.com>
2024-03-19 13:35:09 -0700

    skills updated

--- app/(home)/components/skills.tsx
+++ app/(home)/components/skills.tsx
@@ -2,7 +2,7 @@
 import React from 'react'
 import Title from './Title'
 import { HoverEffect } from '@/components/ui/card-hover-effect'
-import { SiNextdotjs, SiReact, SiTailwindcss } from 'react-icons/si'
+import { SiNextdotjs, SiReact, SiTailwindcss, SiHtml5,SiCss3,SiJavascript,SiExpress,SiMongodb,SiNodedotjs,SiTypescript,SiSupabase,SiPostman,SiPrisma,SiThreedotjs,SiOpenai } from 'react-icons/si'
 
 const skills = () => {
 
@@ -20,29 +20,53 @@ const skills = () => {
             Icon: SiTailwindcss
         },
         {
-            text:"React",
-            Icon: SiReact
+            text:"JavaScript",
+            Icon: SiJavascript
         },
         {
-            text:"Next.Js",
-            Icon: SiNextdotjs
+            text:"TypeScript",
+            Icon: SiTypescript
         },
         {
-            text:"Tailwind",
-            Icon: SiTailwindcss
+            text:"Supabase",
+            Icon: SiSupabase
         },
         {
-            text:"React",
-            Icon: SiReact
+            text:"Node.Js",
+            Icon: SiNodedotjs
         },
         {
-            text:"Next.Js",
-            Icon: SiNextdotjs
+            text:"MongoDB",
+            Icon: SiMongodb
         },
         {
-            text:"Tailwind",
-            Icon: SiTailwindcss
-        }
+            text:"Express",
+            Icon: SiExpress
+        },
+        {
+            text:"HTML",
+            Icon: SiHtml5
+        },
+        {
+            text:"CSS",
+            Icon: SiCss3
+        },
+        {
+            text:"Postman",
+            Icon: SiPostman
+        },
+        {
+            text:"Prisma",
+            Icon: SiPrisma
+        },
+        {
+            text:"Three.js",
+            Icon: SiThreedotjs
+        },
+        {
+            text:"OpenAI",
+            Icon: SiOpenai
+        },
     ]
 
   return (
